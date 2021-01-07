2d6328412a731149519f88af93c0c5dcd3f5dc0c
vivaanparekh28 <74426172+vivaanparekh28@users.noreply.github.com>
2021-01-07 16:15:07 +0000

    Add files via upload

---
+++ Blocks.js
@@ -0,0 +1,30 @@
+class Blocks{
+    constructor(x,y){
+       
+this.body=Bodies.rectangle(x,y,30,50)
+World.add(world,this.body)
+
+this.visible=255
+    }
+display(){
+    if (this.body.speed<3){
+        push()
+        rectMode(CENTER)
+        fill("blue")
+        rect(this.body.position.x,this.body.position.y,30,50)
+        
+        pop()
+    }
+    else{
+        push()
+        this.visible-=5
+    tint(255,this.visible)
+   
+    pop()
+        
+        
+  World.remove(world,this.body)
+    }
+   
+}
+}
